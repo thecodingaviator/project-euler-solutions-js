fa713d24643c82ac2f4b2fd4557252c0ab1cd86b
Parth Parth <34807532+thecodingaviator@users.noreply.github.com>
2020-05-09 14:14:39 +0530

    Create 018-maximum-path-sum-i.js

---
+++ 018-maximum-path-sum-i.js
@@ -0,0 +1,33 @@
+function maximumPathSumI(triangle) {
+  console.clear()
+  let sum=triangle[0][0]
+  let lowerBound=0, upperBound=1
+  let obj={}
+  for(var i=1; i<triangle.length; i++){
+    obj=returnMax(triangle[i], lowerBound, upperBound)
+    console.log(obj)
+    sum+=obj.m
+    lowerBound=obj.lowerBound
+    upperBound=obj.upperBound
+  }
+  return sum
+}
+
+function returnMax(arr, lowerBound, upperBound){
+  let m=0
+  if(arr[lowerBound]>arr[upperBound])
+    m=arr[lowerBound]
+  else if(upperBound==lowerBound) {
+    m=arr[lowerBound]
+  }
+  else {
+    m=arr[upperBound]
+    lowerBound++
+    upperBound++
+  }
+  return {
+    m,
+    lowerBound,
+    upperBound
+  }
+}
