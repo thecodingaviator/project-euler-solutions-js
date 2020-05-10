a84e5b41a8c78c3bed3dc51d96e7f5214d364915
Parth Parth <34807532+thecodingaviator@users.noreply.github.com>
2020-05-10 23:50:07 +0530

    Update 016-power-digit-sum.js

--- 016-power-digit-sum.js
+++ 016-power-digit-sum.js
@@ -6,25 +6,21 @@ function powerDigitSum(exponent) {
     power++
   }
   power=0
-  for(var i=0; i<numArray.length; i++) {
+  for(var i=0; i<numArray.length; i++)
     power+=numArray[i]
-  }
   return power
 }
 
 function updateArray(numArray, len) {
-  for(var i=len-1; i>=0; i--) {
+  for(var i=len-1; i>=0; i--)
     numArray[i]*=2
-  }
   for(var i=len-1; i>=0; i--) {
     if(numArray[i]>9) {
       numArray[i]-=10
-      if(typeof numArray[i-1] == "number"){
-        numArray[i-1]+=1
-      }
-      else {
-        numArray.unshift(1)
-      }
+    if(typeof numArray[i-1] == "number")
+      numArray[i-1]+=1
+    else
+      numArray.unshift(1)
     }
   }
 }
