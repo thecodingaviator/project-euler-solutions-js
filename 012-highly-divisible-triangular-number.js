9ec4ee6f8d770bca51fb728227941ed55c95c738
Parth Parth <34807532+thecodingaviator@users.noreply.github.com>
2020-05-12 00:34:14 +0530

    Update 012-highly-divisible-triangular-number.js

--- 012-highly-divisible-triangular-number.js
+++ 012-highly-divisible-triangular-number.js
@@ -7,6 +7,7 @@ function divisibleTriangleNumber(n) {
   return currentNatural
 }
 
+/* My original code
 function numberOfFactors(n) {
   let count=2
   let i=2
@@ -16,6 +17,28 @@ function numberOfFactors(n) {
       count += 2
     i += 1
   }
+  if (i**2==n)
+    count++
+  return count
+}
+*/
+
+// @JeremyLT on the freeCodeCamp forum helped me with this:
+function numberOfFactors(n) {
+  let count=2
+  let i=2
+  // Single bound computation is faster
+  const bound = Math.floor(Math.sqrt(n))
+  // Check i from 2 to largest possible factor
+  while(i < bound) {
+    if (n % i == 0)
+      // Because factors come in pairs
+      // if i is a factor, n/i is too
+      count += 2
+    i += 1
+  }
+  // If n = i*i, then we only want to count
+  // the factor i once
   if (i**2==n)
     count++
   return count
